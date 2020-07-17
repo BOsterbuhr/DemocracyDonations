8515fa7bdf9caea9724c2a643aa5877940f6d80b
Brody <34581663+CallMeChody@users.noreply.github.com>
2020-07-17 10:52:07 -0500

    Update __init__.py

--- DemocracyDonations/app/__init__.py
+++ DemocracyDonations/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_bcrypt import Bcrypt 
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
